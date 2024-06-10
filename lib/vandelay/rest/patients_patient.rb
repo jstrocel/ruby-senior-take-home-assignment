4a55fe446b22d93df46bc25b82ee093cb0400f28
James Strocel <james.strocel@gmail.com>
2024-06-10 12:22:03 -0700

    Passing single patient test

--- lib/vandelay/rest/patients_patient.rb
+++ lib/vandelay/rest/patients_patient.rb
@@ -4,8 +4,16 @@ require 'vandelay/services/patient_records'
 module Vandelay
   module REST
     module PatientsPatient
+      def self.patients_srvc
+        @patients_srvc ||= Vandelay::Services::Patients.new
+      end
+
       def self.registered(app)
-        # add endpoint code here
+        app.get '/patients/patient/:patient_id' do
+          patient_id = params[:patient_id]
+          result = Vandelay::REST::Patients.patients_srvc.retrieve_one(patient_id)
+          json(result.to_h)
+        end
       end
     end
   end
